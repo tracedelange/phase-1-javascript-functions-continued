29388017c71e22112fdfc1aabd707304cad3ed77
Trace <tracedelange@me.com>
2021-06-07 14:31:15 -0500

    commit tree acea5c1fa2f6689ec10a06f0fd8c7bf863e570a1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrap='*'){
+    return function(adj){
+        return `You are ${wrap}${adj}${wrap}!`
+    }
+}
